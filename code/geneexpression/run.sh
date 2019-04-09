a9b0a605f19c3917a0f2299ad3afff46e6fecbd7
Neha Prasad <nehap@mit.edu>
2019-04-09 10:21:51 -0400

    new changes

--- code/geneexpression/run.sh
+++ code/geneexpression/run.sh
@@ -4,5 +4,5 @@ savedir="./results/exp4/"
 mkdir $savedir
 
 # CUDA_VISIBLE_DEVICES=0 python main.py -bs 128 -sd $savedir -iter 10000 -lamb0 0.01 -lamb1 0.1 -lamb2 1 -lambG2 0.1 -psi "JS" -citer 2 -lrG 0.0001 -lrD 0.0001 -env "geneexpression_exp4" > "${savedir}log.txt"
-CUDA_VISIBLE_DEVICES=0 python main.py -bs 128 -sd $savedir -iter 10000 -lamb0 0.01 -lamb1 0.1 -lamb2 1 -lambG2 0.1 -psi "JS" -citer 2 -lrG 0.0001 -lrD 0.0001 -env "geneexpression_exp4"
+CUDA_VISIBLE_DEVICES=1 python main.py -bs 128 -sd $savedir -iter 400 -lamb0 0.01 -lamb1 0.1 -lamb2 1 -lambG2 0.1 -psi "JS" -citer 2 -lrG 0.0001 -lrD 0.0001 -env "nehaenv" > "${savedir}log.txt"
 # CUDA_VISIBLE_DEVICES=0 python main.py -bs 128 -sd $savedir -iter 10000 -lamb0 0.01 -lamb1 0.1 -lamb2 1 -lambG2 0.1 -psi "JS" -citer 2 -lrG 0.0001 -lrD 0.0001 > "${savedir}log.txt"
